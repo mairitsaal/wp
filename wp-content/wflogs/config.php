7464cf10147328c1a62caa98b394c08cbc62eb62
saalmairitiktkhk <mairit.saal@khk.ee>
2019-12-09 01:40:29 +0200

    modified file `wflogs`

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:4:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"F&o;%^trkE=J^2E5S)aB<%4943?yF~or6MO)e}Q58[=?!/Y|sk7`LDe6>^Ab#_!E";s:7:"version";s:5:"1.0.4";}
