727c5e73adde6d93b73b4eb30fa1bf26b1475833
saalmairitiktkhk <mairit.saal@khk.ee>
2019-12-18 00:01:09 +0200

    installed plugin `Very First Plugin` version 1.2

---
+++ wp-content/plugins/very-first-plugin/very-first-plugin.php
@@ -0,0 +1,18 @@
+<?php
+   /*
+   Plugin Name: Very First Plugin
+   Plugin URI: http://saalmairit.ikt.khk/wordpress
+   description: >-
+  a plugin to create awesomeness and spread joy
+   Version: 1.2
+   Author: M. Saal
+   Author URI: http://saalmairit.ikt.khk/wordpress
+   License: 1.0
+   */
+function dh_modify_read_more_link() {
+    return '<a class="more-link" href="' . get_permalink() . '">Jätka lugemist...</a>';
+}
+add_filter( 'the_content_more_link', 'dh_modify_read_more_link' );
+ 
+ 
+?>
