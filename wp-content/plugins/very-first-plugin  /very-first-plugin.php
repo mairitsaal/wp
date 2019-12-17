03c93c30b6f310a198f3f8844a57ada09d209149
saalmairitiktkhk <mairit.saal@khk.ee>
2019-12-17 23:51:28 +0200

    modified plugin `Very First Plugin` version 1.2

--- wp-content/plugins/very-first-plugin  /very-first-plugin.php
+++ wp-content/plugins/very-first-plugin  /very-first-plugin.php
@@ -9,6 +9,10 @@
    Author URI: http://saalmairit.ikt.khk/wordpress
    License: 1.0
    */
-
+//* Modify the WordPress read more link
+add_filter( 'the_content_more_link', 'sp_read_more_link' );
+function sp_read_more_link() {
+	return '<a class="more-link" href="' . get_permalink() . '">[Jätka lugemist...]</a>';
+}
  
 ?>
