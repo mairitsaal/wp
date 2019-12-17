9af162f1259b1b3948baf85af3f85ec28e5f793a
saalmairitiktkhk <mairit.saal@khk.ee>
2019-12-18 00:16:59 +0200

    modified plugin `My First Plugin` version 1.0

--- wp-content/plugins/very-first-plugin  /very-first-plugin.php
+++ wp-content/plugins/very-first-plugin  /very-first-plugin.php
@@ -1,17 +1,15 @@
 <?php
-   /*
-   Plugin Name: Very First Plugin
-   Plugin URI: http://saalmairit.ikt.khk/wordpress
-   description: >-
-  a plugin to create awesomeness and spread joy
-   Version: 1.2
-   Author: M. Saal
-   Author URI: http://saalmairit.ikt.khk/wordpress
-   License: 1.0
-   */
-function dh_modify_read_more_link() {
-    return '<a class="more-link" href="' . get_permalink() . '">Jätka lugemist...</a>';
+/**
+ * Plugin Name: My First Plugin
+ * Plugin URI: http://saalmairit.ikt.khk/wordpress/my-first-plugin
+ * Description: The very first plugin that I have ever created.
+ * Version: 1.0
+ * Author: Your Name
+ * Author URI: http://saalmairit.ikt.khk/wordpress
+ */
+ add_action( 'the_content', 'my_thank_you_text' );
+
+function my_thank_you_text ( $content ) {
+    return $content .= '<p>Thank you for reading!</p>';
 }
-add_filter( 'the_content_more_link', 'dh_modify_read_more_link' );
- 
- 
+?>
