3cd67e0a85dd563116cbf8556f52b7c32fed7345
saalmairitiktkhk <mairit.saal@khk.ee>
2019-12-17 23:28:27 +0200

    modified plugin `Very First Plugin` version 1.2 (after activation of `Very First Plugin` version 1.2)

---
+++ wp-content/plugins/very-first-plugin  /very-first-plugin.php
@@ -0,0 +1,14 @@
+<?php
+   /*
+   Plugin Name: Very First Plugin
+   Plugin URI: http://saalmairit.ikt.khk/wordpress
+   description: >-
+  a plugin to create awesomeness and spread joy
+   Version: 1.2
+   Author: M. Saal
+   Author URI: http://saalmairit.ikt.khk/wordpress
+   License: 1.0
+   */
+
+ 
+?>
