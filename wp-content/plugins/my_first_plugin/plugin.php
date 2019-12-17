177686abb0271b54c90176a65fb177b3bd96e320
saalmairitiktkhk <mairit.saal@khk.ee>
2019-12-17 16:40:24 +0200

    modified plugin `Change Logo` version 1.2 (after activation of `Change Logo` version 1.2)

---
+++ wp-content/plugins/my_first_plugin/plugin.php
@@ -0,0 +1,13 @@
+<?php
+   /*
+   Plugin Name: Change Logo
+   Plugin URI: http://saalmairit.ikt.khk/wordpress.com
+   description: >-
+  a plugin to create awesomeness and spread joy
+   Version: 1.2
+   Author: M. Saal
+   Author URI: http://saalmairit.ikt.khk/wordpress.com
+   License: 1.0
+   */
+
+?>
